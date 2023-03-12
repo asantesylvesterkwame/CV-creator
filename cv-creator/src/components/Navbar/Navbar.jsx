3b96869eb09179ba2b31c6579c5010a8c6c3837d
asantesylvesterkwame <intelligentson17@gmail.com>
2023-03-12 04:14:55 +0000

    put elements in my style.component

--- cv-creator/src/components/Navbar/Navbar.jsx
+++ cv-creator/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,13 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    
+    <div className="container">
+        <div className='appHolderDiv'>
+            <div className="navBarDiv">
+                <h1 className='nameOfApp'>AMPLIFIED CV - CREATOR APP</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
